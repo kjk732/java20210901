12d060819c2a8c760df1951b126275c580833973
kjk732 <kjk732@gmail.com>
2021-09-01 12:17:02 +0900

    gun

--- src/p01review/C02Polymorphism.java
+++ src/p01review/C02Polymorphism.java
@@ -3,6 +3,42 @@ package p01review;
 public class C02Polymorphism {
 	
 	// 다형성
-	
+	public static void main(String[] args) {
+			
+		Animal a1 = new Animal();
+		Cat c1 = new Cat();
+		Dog d1 = new Dog();
+		
+		a1.cry();
+		c1.cry();
+		d1.cry();
+		
+		System.out.println("=========================");
+		
+		Animal a2 = c1;
+		Animal a3 = d1;
+		
+		a2.cry();
+		a3.cry();
+		
+		//Cat c2 = a1; 안됌
+		//Dog d2 = a1;
+		
+	}
 	
 }
+
+class Animal{
+	public void cry() {
+		System.out.println("동물이 웁니다.");
+	}
+}
+class Cat extends Animal{
+	public void cry() {
+		System.out.println("고양이가 웁니다.");
+	}
+}
+class Dog extends Animal{
+	
+}
+
